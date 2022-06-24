df69fbbfb431c3e6000db59874d76c533afb4676
alravid <allancravid@tecnico.ulisboa.pt>
2022-06-24 23:51:09 +0100

    add queries

--- E3/src/queries.sql
+++ E3/src/queries.sql
@@ -1,6 +1,27 @@
+-- a )
+SELECT nome
+FROM(
+    SELECT tin, nome, COUNT (DISTINCT nome_cat) AS categorias
+    FROM retalhista NATURAL JOIN responsavel_por
+    GROUP BY tin, nome
+) AS foo
+WHERE categorias >= ALL (SELECT COUNT (DISTINCT nome_cat)
+                         FROM retalhista NATURAL JOIN responsavel_por
+                         GROUP BY tin, nome
+);
 
 
+-- b)
 
+
+SELECT nome 
+FROM retalhista
+WHERE tin IN (
+	SELECT tin FROM responsavel_por 
+	WHERE nome_cat IN (
+		SELECT nome FROM categoria_simples
+	)
+);
 
 -- c)
 
